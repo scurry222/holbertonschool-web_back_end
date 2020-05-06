d633d05a9bbea3660a3ba8dbba2be274fd988ef6
scurry222 <scurry222@gmail.com>
2020-05-06 14:28:30 -0700

    format exactly like example

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,6 +9,6 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     ''' Create generator of random values '''
-    for i in range(10):
+    for _i in range(10):
         await asyncio.sleep(1)
         yield 10 * random.random()
