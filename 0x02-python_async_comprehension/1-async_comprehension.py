054fc8ecf72360beb4a17b775e3d79f194600c4a
scurry222 <scurry222@gmail.com>
2020-05-06 14:25:23 -0700

    pep8 compliant

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,4 +11,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     ''' Collect 10 random numbers from generator, return them '''
-    return [i async for i in async_generator()] 
+    return [i async for i in async_generator()]
