44efdabfa43270171cbb0d48580285bce4608299
scurry222 <scurry222@gmail.com>
2020-05-06 11:25:47 -0700

    create tasks to gather asyncronously

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,7 +9,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''  '''
+    ''' Return time of 4 async gather calls '''
+    tasks = []
     total_time = time.time()
-    await asyncio.gather(async_comprehension())
+    for i in range(4):
+        tasks.append(asyncio.create_task(async_comprehension()))
+    await asyncio.gather(*tasks)
     return time.time() - total_time
