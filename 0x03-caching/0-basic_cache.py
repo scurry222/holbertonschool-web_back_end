d1f6ea58607ec06d660da1636d768c014500f71a
scurry222 <scurry222@gmail.com>
2020-05-10 14:34:58 -0700

    explicit key and item is not none check

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -12,7 +12,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key or item:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
