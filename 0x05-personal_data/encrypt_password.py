4e3f6c482b2b517fe891b2b3b1929942b4b7f2b1
scurry222 <scurry222@gmail.com>
2020-05-17 20:04:11 -0700

    compare byte object against hashed object

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -2,17 +2,16 @@
 """
     Password encryption module
 """
-
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """ hash and salt input password  """
     if password:
-        bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Check validity of password """
     if hashed_password and password:
-        return bcrypt.checkpw(password, hashed_password)
+        return bcrypt.checkpw(str.encode(password), hashed_password)
