a0379c9c48ae83661d0d37d6ed02d44236b7c3b8
scurry222 <scurry222@gmail.com>
2020-06-01 14:24:22 -0700

    sizing

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -18,4 +18,4 @@ class User(Base):
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
-    reset_token = Column(String, nullable=True)
+    reset_token = Column(String(250), nullable=True)
