9d70e93279540ed29ccb4858d878117e8cef83ac
scurry222 <scurry222@gmail.com>
2020-06-09 18:03:32 -0700

    add task 5 and change parameters

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 """
 
 from unittest import TestCase, mock
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 from utils import requests
@@ -12,9 +12,8 @@ from utils import requests
 class TestGithubOrgClient(TestCase):
     """ Test cases for client.GithubOrgClient """
     @parameterized.expand([
-        ("github", {"payload": True}),
-        ("twitter", {"payload": True}),
-        ("not a website", {"payload": False}),
+        ("google", {"payload": True}),
+        ("abc", {"payload": False}),
     ])
     @patch('client.get_json')
     def test_org(self, name, payload, mock_get):
@@ -24,3 +23,14 @@ class TestGithubOrgClient(TestCase):
 
         self.assertEqual(payload, instance.org)
         mock_get.assert_called_once()
+
+    def test_public_repos_url(self):
+        """ test cases for client._test_public_repos_url """
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mock_request:
+            mock_request.return_value = {"url": 'http://scoutcurry.com'}
+
+            res = GithubOrgClient(mock_request.return_value)._public_repos_url
+
+            self.assertEqual(res, mock_request.return_value)
+            mock_request.assert_called_once()
