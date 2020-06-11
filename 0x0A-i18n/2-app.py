16eaffe740a89047bb889270d965589ba225f2e0
scurry222 <scurry222@gmail.com>
2020-06-10 19:31:24 -0700

    add flask apps 2, 3

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+from flask_babel import Babel
+import os
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Configuration of available languages """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "fr"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Return best match from accepted languages """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+@app.route("/")
+def home():
+    """Home page"""
+
+    return render_template("2-index.html")
+
+
+# @app.route("/error")
+# def error():
+#     raise Exception("Error!")
+
+if __name__ == "__main__":
+
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
+
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
