748c2a7043385be86185beca6c55defd02b48c3c
scurry222 <scurry222@gmail.com>
2020-06-19 10:21:19 -0700

    change trigger name

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,6 +1,6 @@
 -- Create a trigger for the items table in the holberton database
 -- Decrease the quantity of an item when a new order is inserted
-CREATE TRIGGER trigger_name BEFORE INSERT
+CREATE TRIGGER register_order BEFORE INSERT
 ON orders FOR EACH ROW
 UPDATE items
 SET quantity = quantity - NEW.number
