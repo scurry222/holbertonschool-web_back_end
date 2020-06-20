0398a8e6ba24a8c4b1443308968d053e4894f47b
scurry222 <scurry222@gmail.com>
2020-06-19 18:48:27 -0700

    add command break

--- 0x0C-MySQL_Advanced/10-div.sql
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -2,7 +2,7 @@
 -- the second number or returns 0 if the second number is equal to 0.
 DELIMITER $$
 
-DROP FUNCTION IF EXISTS SafeDiv
+DROP FUNCTION IF EXISTS SafeDiv;
 
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT
